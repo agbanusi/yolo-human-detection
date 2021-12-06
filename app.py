078c6de2c8c6048a50b1ce449fcafe1731513e8c
John Agbanusi <agbanusijohn@gmail.com>
2021-12-06 08:56:25 +0100

    new update

--- app.py
+++ app.py
@@ -24,13 +24,12 @@ def get_detections_by_image_files():
     #convert string data to numpy array
     npimg = np.fromfile(filestr, np.uint8)
     img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-    img = cv2.resize(img, (416, 416))
     print(img)
 
     # create list for final response
     responses =[]
    
-    classIds, scores, boxes = model.detect(img, confThreshold=0.5, nmsThreshold=0.25)
+    classIds, scores, boxes = model.detect(img, confThreshold=0.55, nmsThreshold=0.35)
     print(classIds, scores)
     
     for (classId, score, box) in zip(classIds, scores, boxes):
@@ -75,7 +74,7 @@ def get_detections_by_alternate():
                     scores = det[5:]
                     classId = np.argmax(scores)
                     confidence = scores[classId]
-                    if confidence > confThreshold:
+                    if confidence > 0.55:
                             w,h = int(det[2]* wT), int(det[3]*hT)
                             x,y = int((det[0]*wT)-w/2), int((det[1]*hT)-h/2)
                             responses.append({
